68a60898a103bc2db00a9a95a4b6dc5f2bb60467
Daniel Orozco Venegas <b24882@ecci.ucr.ac.cr@ECCI-LBL-204-11.ecci.ucr.ac.cr>
2017-05-19 14:51:25 -0600

    scanner basico sin reconocer keywords

--- scanner.f
+++ scanner.f
@@ -1,28 +1,56 @@
 %{
+	#include <stdio.h>
+
 	/*Comentarios*/
-	int nums = 0;
-	int strs = 0;
+	int numNumeros = 0;
+	int numIdentificadores = 0;
+	int numKeywords = 0;
 %}
 
 %option noyywrap
 
-NUM		[0-9]
+DIGITO [0-9]
+LETRA	[a-zA-Z]
+KEYWORD "if"|"then"|"else"|"fi"
+IDENTIFICADOR	^{LETRA}(({LETRA}|{DIGITO})*)$
+NUMERO ^{DIGITO}+(\.{DIGITO}+)?([Ee][+-]?{DIGITO}+)?
 
 %%
 "fin"	{
-			printf ("NUMS: %d\nSTRS: %d", nums, strs);
+			fprintf (yyout ,"Se reconocieron %d números\n", numNumeros);
+			fprintf (yyout ,"Se reconocieron %d palabras reservadas\n", numKeywords);
+			fprintf (yyout ,"Se reconocieron %d identificadores\n", numIdentificadores);
 			return 0;
 		}
-{NUM}+	{
-			printf ("%s es NUM\n", yytext);
-			nums++;
+{NUMERO}	{
+			fprintf (yyout ,"%s es un número\n", yytext);
+			numNumeros++;
 		}
-[a-z]+  {
-			printf ("%s es STR\n", yytext);
-			strs++;
+{KEYWORD}	{
+			fprintf (yyout ,"%s es una palabra reservada\n", yytext);
+			numKeywords++;
+		}
+{IDENTIFICADOR}  {
+			fprintf (yyout ,"%s es un identificador\n", yytext);
+			numIdentificadores++;
+		}
+.* {
+
 		}
 %%
 
-int main (void) {
-	yylex();
+int main (int argc, char** argv) {
+	if(argc >= 1){
+		yyin = fopen(argv[1], "r");
+		if(argc == 3){
+			yyout = fopen(argv[2], "w");
+		}
+		else{
+			yyout = fopen("salida.txt", "w");
+		}
+		yylex();
+	}
+	else{
+		printf("No se ingresaron los argumentos necesarios");
+	}
 }
